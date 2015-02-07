c2cdfdc156732392210be44f74bdb9113cf1c436
Hoang Kha <hoangkha252|@gmail.com>
2015-02-07 12:47:35 +0700

    update model

--- app/models/User.php
+++ app/models/User.php
@@ -1,4 +1,70 @@
-<?php 
-class User extends Eloquent {
-	protected $table= 'users';
+<?php
+
+use Illuminate\Auth\UserTrait;
+use Illuminate\Auth\UserInterface;
+use Illuminate\Auth\Reminders\RemindableTrait;
+use Illuminate\Auth\Reminders\RemindableInterface;
+
+class User extends Eloquent implements UserInterface, RemindableInterface {
+
+	use UserTrait, RemindableTrait;
+
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'users';
+
+	/**
+	 * The attributes excluded from the model's JSON form.
+	 *
+	 * @var array
+	 */
+	protected $hidden = array('password', 'remember_token');
+	 public function getByPage($page = 1, $limit = 3)
+	    {
+	      $results = new StdClass;
+	      $results->page = $page;
+	      $results->limit = $limit;
+	      $results->totalItems = 0;
+	      $results->items = array();
+	     
+	      $users = User::skip($limit*($page-1))->take($limit)->get();
+	     
+	      $results->totalItems = User::count();
+	      $results->items = $users->all();
+	     
+	      return $results;
+	    }
+	public function getRememberToken()
+	{
+		return $this->remember_token;
+	}
+
+	public function setRememberToken($value)
+	{
+		$this->remember_token = $value;
+	}
+
+	public function getRememberTokenName()
+	{
+		return 'remember_token';
+	}
+	
+	public function getRememberToken()
+	{
+		return $this->remember_token;
+	}
+
+	public function setRememberToken($value)
+	{
+		$this->remember_token = $value;
+	}
+
+	public function getRememberTokenName()
+	{
+		return 'remember_token';
+	}
+
 }
