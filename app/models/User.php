9664f1fab421c54214d780254a032210166520f9
Hoang Kha <hoangkha252|@gmail.com>
2015-02-07 13:04:38 +0700

    getbypage

--- app/models/User.php
+++ app/models/User.php
@@ -22,5 +22,20 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 	 * @var array
 	 */
 	protected $hidden = array('password', 'remember_token');
+	public function getByPage($page = 1, $limit = 3)
+	    {
+	      $results = new StdClass;
+	      $results->page = $page;
+	      $results->limit = $limit;
+	      $results->totalItems = 0;
+	      $results->items = array();
+	     
+	      $users = User::skip($limit*($page-1))->take($limit)->get();
+	     
+	      $results->totalItems = User::count();
+	      $results->items = $users->all();
+	     
+	      return $results;
+	    }
 
 }
