2ea06d584b755c6b11a3ebcab20b3d7ab265f38e
System User <landlord1@shell3.gridhost.co.uk>
2015-02-07 03:31:34 +0000

    testing -server2

--- app/config/database.php
+++ app/config/database.php
@@ -54,10 +54,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => '127.0.0.1',
+			'host'      => '10.168.1.83',
 			'database'  => 'landlord1_data',
-			'username'  => 'root',
-			'password'  => '',
+			'username'  => 'landlord1_data',
+			'password'  => 'myrepairs008',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
